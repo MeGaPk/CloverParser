2f22992676dd821d34d6fc4e04e739265cb1ded7
Ivan Gaydamakin <ivan.gaydamakin@modera.ee>
2019-08-02 21:42:44 +0300

    Initial Commit

---
+++ ParserPlist/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ParserPlist
+//
+//  Created by Ivan Gaydamakin on 02.08.2019.
+//  Copyright © 2019 Ivan Gaydamakin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
